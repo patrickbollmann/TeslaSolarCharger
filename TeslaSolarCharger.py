341a9dac498b85c62a23727fe95e3805a060b982
Patrick Bollmann <39247927+patrickbollmann@users.noreply.github.com>
2020-07-17 15:37:22 +0200

    fixed an error where the car would not wake up
    fixed an error where the car would not wake up after sleep

--- TeslaSolarCharger.py
+++ TeslaSolarCharger.py
@@ -10,20 +10,16 @@ pvpassword = 'pv server password'
 charging = 0
 threshold = 3000 #power threshold in watts at which charging the EV should be started
 
-car = myTesla.connect(teslaemail, teslapassword)  #connect to vehicle using myTesla api
-
 while True:
-    try:    #try catch is used to catch server errors and connection problems
+    try:
         r = requests.get(pvhost, auth=(pvuser, pvpassword)
-                         )  # get piko solar webinterface
+                        )  # get piko solar webinterface
         soup = BeautifulSoup(r.text, 'html.parser')
 
         # find power produced at this moment in watts
         power = soup.find_all("td")[14].text.strip()
-        
     except Exception as e:
         print(e)
-        
     if(power == "x x x"):  # convert text to int value
         solarpower = 0
     else:
@@ -32,8 +28,9 @@ while True:
     print("solar power is: "+str(solarpower)+" W")
 
     if (solarpower >= threshold and charging == 0):  # if enough power available start charging
+        car = myTesla.connect(teslaemail, teslapassword)
         car.wake_up
-        time.sleep(15) # wait for car to wake
+        time.sleep(15)
         car.charge_start()
         charging = 1
         print("started charging")
@@ -41,6 +38,7 @@ while True:
         print("charging...")
 
     if (solarpower < threshold and charging == 1):  # if not enough power available stop charging
+        car = myTesla.connect(teslaemail, teslapassword)
         car.wake_up
         car.charge_stop()
         charging = 0
@@ -48,4 +46,4 @@ while True:
     if (solarpower < threshold and charging == 0):
         print("not charging")
 
-    time.sleep(20)  # wait 20 seconds before next power check
+    time.sleep(30)  # wait 30 seconds before next power check
