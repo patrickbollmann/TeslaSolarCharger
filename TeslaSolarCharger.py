f2f1057455f61d71db5f3ece8e1eaa3e45e788b4
Patrick Bollmann <39247927+patrickbollmann@users.noreply.github.com>
2020-07-17 11:33:42 +0200

    small improvements

--- TeslaSolarCharger.py
+++ TeslaSolarCharger.py
@@ -13,12 +13,17 @@ threshold = 3000 #power threshold in watts at which charging the EV should be st
 car = myTesla.connect(teslaemail, teslapassword)  #connect to vehicle using myTesla api
 
 while True:
-    r = requests.get(pvhost, auth=(pvuser, pvpassword)
-                     )  # get piko solar webinterface
-    soup = BeautifulSoup(r.text, 'html.parser')
+    try:    #try catch is used to catch server errors and connection problems
+        r = requests.get(pvhost, auth=(pvuser, pvpassword)
+                         )  # get piko solar webinterface
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-    # find power produced at this moment in watts
-    power = soup.find_all("td")[14].text.strip()
+        # find power produced at this moment in watts
+        power = soup.find_all("td")[14].text.strip()
+        
+    except Exception as e:
+        print(e)
+        
     if(power == "x x x"):  # convert text to int value
         solarpower = 0
     else:
@@ -28,6 +33,7 @@ while True:
 
     if (solarpower >= threshold and charging == 0):  # if enough power available start charging
         car.wake_up
+        time.sleep(15) # wait for car to wake
         car.charge_start()
         charging = 1
         print("started charging")
